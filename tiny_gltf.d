3f0f6bb3d3a5e1727b52df31893d36f37958fa02
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-02-21 23:29:51 -0500

    Fix moar

--- tiny_gltf.d
+++ tiny_gltf.d
@@ -1354,7 +1354,7 @@ bool FileExists(const(std) abs_filename, void*);
 /// @param[in] filepath File path string. Assume UTF-8
 /// @param[in] userdata User data. Set to `nullptr` if you don't need it.
 ///
-string ExpandFilePath(const string &filepath, void *userdata);
+string ExpandFilePath(const ref string filepath, void *userdata);
 
 bool ReadWholeFile(ubyte* out_, std* err, const(std) filepath, void*);
 
