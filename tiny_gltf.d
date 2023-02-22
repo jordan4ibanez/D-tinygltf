7b5b73e79a125be648813cf6a6f790ff2380a7ce
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-02-21 22:04:07 -0500

    BufferView

--- tiny_gltf.d
+++ tiny_gltf.d
@@ -842,31 +842,33 @@ struct Material {
 }
 
 struct BufferView {
-  std;/*::string name !!*/
-  int buffer;        // Required
-  size_t byteOffset;  // minimum 0, default 0
-  size_t byteLength;  // required, minimum 1. 0 = invalid
-  size_t byteStride;  // minimum 4, maximum 252 (multiple of 4), default 0 =
-                         // understood to be tightly packed
-  int target;  // ["ARRAY_BUFFER", "ELEMENT_ARRAY_BUFFER"] for vertex indices
-                  // or attribs. Could be 0 for other data
-  Value extras;
-  ExtensionMap extensions;
+    string name;
+    int buffer = -1;        // Required
+    size_t byteOffset = 0;  // minimum 0, default 0
+    size_t byteLength = 0;  // required, minimum 1. 0 = invalid
+    size_t byteStride = 0;  // minimum 4, maximum 252 (multiple of 4), default 0 =
+                            // understood to be tightly packed
+    int target = 0;  // ["ARRAY_BUFFER", "ELEMENT_ARRAY_BUFFER"] for vertex indices
+                     // or attribs. Could be 0 for other data
+    Value extras;
+    ExtensionMap extensions;
 
-  // Filled when SetStoreOriginalJSONForExtrasAndExtensions is enabled.
-  std;/*::string extras_json_string !!*/
-  std;/*::string extensions_json_string !!*/
+    // Filled when SetStoreOriginalJSONForExtrasAndExtensions is enabled.
+    string extras_json_string;
+    string extensions_json_string;
 
-  bool dracoDecoded;  // Flag indicating this has been draco decoded
+    bool dracoDecoded = false;  // Flag indicating this has been draco decoded
 
-  ;/*: buffer(-1),
-        byteOffset(0),
-        byteLength(0),
-        byteStride(0),
-        target(0),
-        dracoDecoded(false) {}
-  DEFAULT_METHODS(BufferView)
-  bool_ operator==cast(const(BufferView) &) const !!*/
+    this() {
+        
+    }/*: buffer(-1),
+            byteOffset(0),
+            byteLength(0),
+            byteStride(0),
+            target(0),
+            dracoDecoded(false) {}
+    DEFAULT_METHODS(BufferView)
+    bool_ operator==cast(const(BufferView) &) const !!*/
 }
 
 struct Accessor {
